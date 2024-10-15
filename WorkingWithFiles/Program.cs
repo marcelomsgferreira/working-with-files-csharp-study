613bb788a153cadbd069e9ff6cf86d868bbf4a62
Marcelo Ferreira <marcelo.msgf.pro@gmail.com>
2024-10-14 23:19:46 -0300

    files/v1

---
+++ WorkingWithFiles/Program.cs
@@ -0,0 +1,22 @@
+﻿string nomeArquivo = "eusouumarquivo.txt";
+string relPath = Path.Combine("E:/TI/DotNET/Linguagem/C#/EstudoLibs/WorkingWithFiles/WorkingWithFiles/WorkingWithFiles/Testes", nomeArquivo);
+
+Console.WriteLine(relPath);
+
+string textoASerInserido = "Eu sou um texto novo";
+
+if (File.Exists(relPath))
+{
+    Console.WriteLine("O Arquivo Existe");
+    await File.AppendAllTextAsync(relPath, textoASerInserido);
+
+    Console.WriteLine("Texto Inserido");
+
+    string textoLido = await File.ReadAllTextAsync(relPath);
+
+    Console.WriteLine(textoLido);
+}
+else
+{
+    Console.WriteLine("Arquivo Não Encontrado");
+}
