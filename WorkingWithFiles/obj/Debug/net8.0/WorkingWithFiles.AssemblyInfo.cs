b40e0b57c5b62b498aff95f7ff0c5257891ec488
Marcelo Ferreira <marcelo.msgf.pro@gmail.com>
2024-10-15 20:14:18 -0300

    Estudo Finalizado - File, Directory, Path e Streams

--- WorkingWithFiles/obj/Debug/net8.0/WorkingWithFiles.AssemblyInfo.cs
+++ WorkingWithFiles/obj/Debug/net8.0/WorkingWithFiles.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkingWithFiles")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+613bb788a153cadbd069e9ff6cf86d868bbf4a62")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkingWithFiles")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkingWithFiles")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
